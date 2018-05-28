fae7d4e55a304275e619929a6c50032882eec0a0
Alberto Gonzalez <alberto.gonzalez@uji.es>
2018-05-28 21:33:51 +0200

    Initial game interface defined

--- SnakeCanvas/MainWindow.xaml.cs
+++ SnakeCanvas/MainWindow.xaml.cs
@@ -24,5 +24,10 @@ namespace SnakeCanvas
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
